9a112549d490757b45fd6177e2e0994690029b4b
saeed99-dev <saeedhabib1999@gmail.com>
2026-02-02 17:04:36 +0530

    add to update Qualification and Passing Year

--- Indixpert_task.py/Menu.py
+++ Indixpert_task.py/Menu.py
@@ -2,29 +2,45 @@ listdata=[]
 while True:
     print("1. Registration ")
     print("2. Search Student Records ")
-    print("3. Exit ")
-    choice=int(input("Please Enter Your Choice: "))
-
-    if choice==1:
-        student={
-                "name":input("Please Enter student's name: "),
-                "id":int(input("Please Enter student's id: ")),
-                "email":input("Please Enter student's emai: "),
-                "address":input("Please Enter student's address: ")
-            }
-        listdata.append(student)
-        print(listdata)
-    elif choice==2:
-        search_id=int(input("Please Enter Student ID to search for "))
-        found_student=False
-        for student in listdata:
-            if student["id"]==search_id:
-                found_student=student
+    print("3. Display Information")
+    print("4. Exit ")
+    input_choice=input("Please Enter Your Choice: ")
+    if input_choice.isdigit():
+        choice=int(input_choice)
+        if 1<=choice<=4:
+            if choice==1:
+                student={
+                        "name":input("Please Enter student's name: "),
+                        "id":int(input("Please Enter student's id: ")),
+                        "email":input("Please Enter student's emai: "),
+                        "address":input("Please Enter student's address: "),
+                        "Qualification":[]
+                    }
+                listdata.append(student)
+                while True:
+                    add_qual=input("Do you want to Add qualification? (yes/no): ").lower()
+                    if add_qual!="yes":
+                        break
+                    input_qualification={
+                        "Passing_Year":int(input("Please Enter Passing Year: ")),
+                        "qual_name":input("Please Enter name of Qualification: ")
+                    }
+                    student["Qualification"].append(input_qualification)
+            elif choice==2:
+                search_id=int(input("Please Enter Student ID to search for "))
+                for student in listdata:
+                    if student["id"]==search_id:
+                        print(f"\nDetail Found for student ID = {student["id"]}")
+                        print(f"-----Searched Student's Details-----\n{student}")
+                        break   
+                else:
+                    print("Student's ID not Found!")
+            elif choice==3:
+               print(listdata)
+            else:
+                print("Exiting Program....")
                 break
-        
-        if found_student:
-            print(f"\nDetail Found for student ID = {student["id"]}\n-----Searched Student's Details-----\n{found_student}")
         else:
-            print("Student's ID not Found!")
-    elif choice==3:
-        break
+            print("Please Enter Whole number between 1-4 only.")
+    else:
+        print("INvalid Input! Please Enter Number only. ")
